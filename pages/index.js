1c03761ff0bc539d468e9276e17a06a511a0fef5
m_irfan_arshad <mirfanarshad17@gmail.com>
2022-04-19 11:00:19 -0400

    HPR-23: merge conflict

--- pages/index.js
+++ pages/index.js
@@ -79,7 +79,9 @@ export default function Home() {
           </Typography>
           <Typography variant="h4">h4 variant mapped to h2</Typography>
         </ThemeProvider>
-        <h1 className={styles.title}>Hello Hopper! Deployed on Cloud Run!</h1>
+        <h1 className={styles.title}>
+          Hello Hopper! Deployed on Cloud Run!
+        </h1>
       </main>
     </div>
   );
