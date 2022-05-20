235646a926dc1bdbe35fccb32a7b90a7e0b90dc8
m_irfan_arshad <mirfanarshad17@gmail.com>
2022-05-20 08:21:30 -0600

    initial launchDarkly implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,17 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "@mui/material/styles";
 import { defaultTheme } from "../reference";
-import { SplitFactory, withSplitFactory } from '@splitsoftware/splitio-react';
-
-const sdkConfig = {
-  core: {
-    authorizationKey: 'm4b8gaq7o4j7929fis12l0003t5bppg5f1cn',
-    key: 'key'
-  }
-};
+import { withLDProvider } from 'launchdarkly-react-client-sdk';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SplitFactory config={sdkConfig} >
       <ThemeProvider theme={defaultTheme}>
         <Component {...pageProps} />
       </ThemeProvider>
-    </SplitFactory>
   );
 }
 
-export default MyApp;
+export default withLDProvider({
+  // TODO: extract this into environment variable
+  clientSideID: '627d9222b1494a150e8f76ba'
+})(MyApp);
