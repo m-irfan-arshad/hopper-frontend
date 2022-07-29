0876b14e4983a8ce710d141e71a0923ede175f26
m_irfan_arshad <mirfanarshad17@gmail.com>
2022-07-29 12:28:36 -0400

    HPR-140: Write tests

---
+++ components/__tests__/caseSummaryDialog.test.tsx
@@ -0,0 +1,143 @@
+import { render, fireEvent } from "@testing-library/react";
+import CaseSummaryDialog from "../caseSummaryDialog";
+import * as R from 'ramda';
+
+describe("CaseSummaryDialog", () => {
+  const props = {
+    open: true,
+    closeDialog: jest.fn(),
+    row:  {
+        caseID: "caseID",
+        firstName: "firstName",
+        lastName: "lastName",
+        dateOfBirth: "01/01/2020",
+        procedureDate: "01/30/1990",
+        procedureLocation: "procedureLocation",
+        proceduralist: "proceduralist",
+        patientAddress: "Grand Line",
+        specialNeeds: "needs",
+        mobilePhone: "111-111-1111",
+        homePhone: "999-999-9999",
+        allergies: "allergies",
+        surgeryLength: "1 hour",
+        comments: "comment",
+        admissionType: "Inpatient",
+        surgeryAssistance: "Doctor Assist",
+        procedures: "Foot Surgery",
+        notes: "note",
+        mrn: "mrn",
+        steps: [
+            {
+                text: "hi",
+                status: false
+            }
+        ]
+    }
+  };
+
+  beforeEach(() => {
+    props.closeDialog.mockReset();
+  });
+
+  test("renders the caseSummaryDialog", () => {
+    const { getByRole } = render(
+      <CaseSummaryDialog {...props} />
+    );
+
+    expect(getByRole('heading', {name: 'Patient Name'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'firstName lastName'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'DOB'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: '01/01/2020'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Patient Address'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'Grand Line'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Special Needs'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'needs'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Patient Phone'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'Mobile: 111-111-1111'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'Home: 999-999-9999'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Allergies'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'allergies'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Procedure Date and Time'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: '01/30/1990'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Length of Surgery'})).toBeInTheDocument();//
+    expect(getByRole('heading', {name: '1 hour'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Comments'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'comment'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Site'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'procedureLocation'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Surgeon Name'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'proceduralist'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Admission Type'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'Inpatient'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Surgical Assistance'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'Doctor Assist'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Procedures'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'Foot Surgery'})).toBeInTheDocument();
+
+    expect(getByRole('heading', {name: 'Anesthesia Notes'})).toBeInTheDocument();
+    expect(getByRole('heading', {name: 'note'})).toBeInTheDocument();
+  });
+
+  test("renders the caseSummaryDialog with N/A for all applicable fields", () => {
+    const propsClone = {
+        open: true,
+        closeDialog: jest.fn(),
+        row:  {
+            caseID: "caseID",
+            firstName: "firstName",
+            lastName: "lastName",
+            dateOfBirth: "01/01/2020",
+            procedureDate: "01/30/1990",
+            procedureLocation: "procedureLocation",
+            proceduralist: "proceduralist",
+            mrn: "mrn",
+            steps: [
+                {
+                    text: "hi",
+                    status: false
+                }
+            ]
+        }
+    };
+   
+    const { getAllByRole } = render(
+      <CaseSummaryDialog {...propsClone} />
+    );
+
+    expect(getAllByRole('heading', {name: 'N/A'})[0]).toBeInTheDocument();
+    expect(getAllByRole('heading', {name: 'N/A'})[1]).toBeInTheDocument();
+    expect(getAllByRole('heading', {name: 'N/A'})[2]).toBeInTheDocument();
+    expect(getAllByRole('heading', {name: 'N/A'})[3]).toBeInTheDocument();
+    expect(getAllByRole('heading', {name: 'N/A'})[4]).toBeInTheDocument();
+    expect(getAllByRole('heading', {name: 'N/A'})[5]).toBeInTheDocument();
+    expect(getAllByRole('heading', {name: 'N/A'})[6]).toBeInTheDocument();
+    expect(getAllByRole('heading', {name: 'N/A'})[7]).toBeInTheDocument();
+    expect(getAllByRole('heading', {name: 'N/A'})[8]).toBeInTheDocument();
+  });
+
+  test("handles closing of createCaseDialog", () => {
+    const { getByRole } = render(
+      <CaseSummaryDialog {...props} />
+    );
+
+    expect(getByRole('button', {name: 'Cancel'})).toBeInTheDocument();
+    expect(props.closeDialog).toHaveBeenCalledTimes(0);
+
+    fireEvent.click(getByRole('button', {name: 'Cancel'}));
+    expect(props.closeDialog).toHaveBeenCalledTimes(1);
+  });
+
+});
---
+++ components/caseSummaryDialog.tsx
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import { 
+    DateRange as DateRangeIcon,
+    AccountBox as AccountBoxIcon,
+    Assignment as AssignmentIcon,
+    Reply as ReplyIcon
+} from "@mui/icons-material";
+import { 
+    Typography, 
+    Grid, 
+    Button, 
+    TextField, 
+    Dialog, 
+    DialogActions, 
+    DialogContent, 
+    DialogTitle,
+    InputLabel,
+    Box
+} from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { SingleCase } from "../reference";
+
+interface Props {
+    open: boolean
+    closeDialog: () => void,
+    row: SingleCase
+}
+
+export default function CaseSummaryDialog(props: Props) {
+  const {open, closeDialog, row} = props;
+
+  return (
+      <Dialog fullWidth open={open} maxWidth="sm" sx={{ "& .MuiPaper-root": { borderRadius: "0.625rem" }}}>
+        <DialogTitle 
+            sx={{
+                backgroundColor: "blue.dark", 
+                color: "white.main",
+                fontSize: "1rem",
+                fontWeight: "400",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center"
+            }}>
+            <Box sx={{display: "flex", flexDirection: "column"}}>
+                <Typography variant="h2">{`${row.lastName}, ${row.firstName}`}</Typography>
+                <Typography
+                    variant="title2"
+                >
+                    {`${row.dateOfBirth} - ${row.mrn}`}
+                </Typography>
+            </Box>
+            <Button 
+                variant="contained" 
+                endIcon={<ReplyIcon sx={{transform: "scaleX(-1)", height: "16px", width: "16px"}} />}
+                sx={{
+                    paddingLeft: "10px",
+                    paddingRight: "10px",
+                    backgroundColor: "green.main",
+                    border: 1,
+                    borderColor: "green.dark",
+                    borderRadius: "0.313rem"
+                }}>
+                    View Full Case
+            </Button>
+        </DialogTitle>
+        <DialogContent>
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+                <Typography variant="title1" color="blue.dark" sx={{display: "flex", alignItems: "center", marginTop: "2rem", marginBottom: "0.75rem"}}>                
+                    <AccountBoxIcon sx={{marginRight: "0.313rem"}}/>
+                    PATIENT INFORMATION
+                </Typography>
+                <Grid container spacing={"2.5rem"}>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Patient Name</Typography>
+                        <Typography variant="subtitle2">{`${row.firstName} ${row.lastName}`}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">DOB</Typography>
+                        <Typography variant="subtitle2">{row.dateOfBirth || 'N/A'}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Patient Address</Typography>
+                        <Typography variant="subtitle2">{row.patientAddress || 'N/A'}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Special Needs</Typography>
+                        <Typography variant="subtitle2">{row.specialNeeds || 'N/A'}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Patient Phone</Typography>
+                        <Typography variant="subtitle2">{`Mobile: ${row.mobilePhone || 'N/A'}`}</Typography>
+                        <Typography variant="subtitle2">{`Home: ${row.homePhone || 'N/A'}`}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Allergies</Typography>
+                        <Typography variant="subtitle2">{row.allergies || 'N/A'}</Typography>
+                    </Grid>
+                </Grid>
+                <Typography variant="title1" color="blue.dark" sx={{display: "flex", alignItems: "center", marginTop: "2rem", marginBottom: "0.75rem"}}>                
+                    <DateRangeIcon sx={{marginRight: "0.313rem"}}/>
+                    SCHEDULING 
+                </Typography>                
+                <Grid container spacing={"2.5rem"}>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Procedure Date and Time</Typography>
+                        <Typography variant="subtitle2">{row.procedureDate || 'N/A'}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Length of Surgery</Typography>
+                        <Typography variant="subtitle2">{row.surgeryLength || 'N/A'}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Comments</Typography>
+                        <Typography variant="subtitle2">{row.comments || 'N/A'}</Typography>
+                    </Grid>
+                </Grid>
+                <Typography variant="title1" color="blue.dark" sx={{display: "flex", alignItems: "center", marginTop: "2rem", marginBottom: "0.75rem"}}>                
+                    <AssignmentIcon sx={{marginRight: "0.313rem"}}/>
+                    PROCEDURE DETAILS 
+                </Typography>
+                <Grid container spacing={"2.5rem"}>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Site</Typography>
+                        <Typography variant="subtitle2">{row.procedureLocation || 'N/A'}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Surgeon Name</Typography>
+                        <Typography variant="subtitle2">{row.proceduralist || 'N/A'}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Admission Type</Typography>
+                        <Typography variant="subtitle2">{row.admissionType || 'N/A'}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Surgical Assistance</Typography>
+                        <Typography variant="subtitle2">{row.surgeryAssistance || 'N/A'}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Procedures</Typography>
+                        <Typography variant="subtitle2">{row.procedures || 'N/A'}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="subtitle1">Anesthesia Notes</Typography>
+                        <Typography variant="subtitle2">{row.notes || 'N/A'}</Typography>
+                    </Grid>
+                </Grid> 
+            </LocalizationProvider>
+        </DialogContent>
+        <DialogActions sx={{display: "flex", justifyContent: "flex-start", alignItems: "center", borderTop: "0.063rem solid", padding: "0.625rem", borderColor: "blue.light"}}>
+          <Button 
+            onClick={() => closeDialog()}
+            sx={{ color: "blue.main", padding: "0.625rem"}}
+          >
+                Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+  );
+}
