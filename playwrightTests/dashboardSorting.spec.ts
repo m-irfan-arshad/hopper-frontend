3984e28ce832347a7df35dac75b2d7e0a1c1994c
m_irfan_arshad <mirfanarshad17@gmail.com>
2022-08-03 09:42:13 -0400

    HPR-143: Fix up e2e tests

--- playwrightTests/dashboardSorting.spec.ts
+++ playwrightTests/dashboardSorting.spec.ts
@@ -19,45 +19,45 @@ test.describe('Landing Page Features', () => {
 
     test('Validating Sort Drop Down Elements', async ({ page }) => {
         const sortDropDownArrow = page.locator("#case-sort-select");
-        const sortOne = page.locator("#menu->>text='Sort: Oldest - Newest'");
-        const sortTwo = page.locator("#menu->>text='Sort: Newest - Oldest'");
+        const sortOne = page.locator("#menu->>text='Oldest - Newest'");
+        const sortTwo = page.locator("#menu->>text='Newest - Oldest'");
         await sortDropDownArrow.click();
-        await expect(sortOne).toContainText('Sort: Oldest - Newest');
+        await expect(sortOne).toContainText('Oldest - Newest');
         await sortOne.click();
         const sortOneDropDownArrowDisplay = page.locator("#case-sort-select");
         await expect(sortOneDropDownArrowDisplay).toContainText('Sort: Oldest - Newest');
         await sortDropDownArrow.click();
-        await expect(sortTwo).toContainText('Sort: Newest - Oldest');
+        await expect(sortTwo).toContainText('Newest - Oldest');
         await sortTwo.click();
         const sortTwoDropDownArrowDisplay = page.locator("#case-sort-select");
         await expect(sortTwoDropDownArrowDisplay).toContainText('Sort: Newest - Oldest');
     });
 
     test('Validating Date Range Drop Down Elements', async ({ page }) => {
-        const dateOne = page.locator("#menu->>text='Date Range: This month'");
-        const dateTwo = page.locator("#menu->>text='Date Range: Next month'");
-        const dateThree = page.locator("#menu->>text='Date Range: Next quarter'");
+        const dateOne = page.locator("#menu->>text='This month'");
+        const dateTwo = page.locator("#menu->>text='Next month'");
+        const dateThree = page.locator("#menu->>text='Next quarter'");
         const dateDropDownArrow = page.locator("#case-date-select");
         await page.waitForTimeout(800);
         await dateDropDownArrow.click();
-        await expect(dateOne).toContainText('Date Range: This month');
-        await expect(dateTwo).toContainText('Date Range: Next month');
-        await expect(dateThree).toContainText('Date Range: Next quarter');
+        await expect(dateOne).toContainText('This month');
+        await expect(dateTwo).toContainText('Next month');
+        await expect(dateThree).toContainText('Next quarter');
         await dateThree.click();
         await expect(dateDropDownArrow).toContainText('Date Range: Next quarter');
 
     });
 
     test('Validating Step Drop Down Elements', async ({ page }) => {
-        const stepOne = page.locator("#menu->>text='Step: All Steps'");
-        const stepTwo = page.locator("#menu->>text='Step: Insurance Authorization'");
-        const stepThree = page.locator("#menu->>text='Step: Pre Surgical Testing'");
+        const stepOne = page.locator("#menu->>text='All Steps'");
+        const stepTwo = page.locator("#menu->>text='Insurance Authorization'");
+        const stepThree = page.locator("#menu->>text='Pre Surgical Testing'");
         const stepDropDownArrow = page.locator("#case-step-select");
         await page.waitForTimeout(800);
         await stepDropDownArrow.click();
-        await expect(stepOne).toContainText('Step: All Steps');
-        await expect(stepTwo).toContainText('Step: Insurance Authorization');
-        await expect(stepThree).toContainText('Step: Pre Surgical Testing');
+        await expect(stepOne).toContainText('All Steps');
+        await expect(stepTwo).toContainText('Insurance Authorization');
+        await expect(stepThree).toContainText('Pre Surgical Testing');
         await stepThree.click();
         await expect(stepDropDownArrow).toContainText('Step: Pre Surgical Testing');
 
