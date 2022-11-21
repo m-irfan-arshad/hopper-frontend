37c37ff262ad5a1b1ed1f0f190bb3b5830cbeaf0
m_irfan_arshad <mirfanarshad17@gmail.com>
2022-11-21 15:52:45 -0500

    HPR-268: Provider Location DB Schema updates

--- pages/api/getLocationOptions.ts
+++ pages/api/getLocationOptions.ts
@@ -2,15 +2,27 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../prisma/clientInstantiation';
 
 export default async function getLocationOptionsHandler(req: NextApiRequest, res: NextApiResponse) {
-    const locationIds = req.query['locationIds'] && (<string>req.query['locationIds']).split(',').map(locationId => {
-        return parseInt(locationId);
-    });
-
-    const locations = await prisma.locations.findMany({
+    const locationProviderRelations = await prisma.provider_locations.findMany({
         where: {
-            locationId: {in: locationIds || []},
+            providerId: {
+                equals: parseInt(<string>req.query['providerId'])
+            }
         }
     });
 
-  res.json(locations)
+    if (locationProviderRelations.length > 0) {
+        const locationIds = locationProviderRelations.map(relation => {
+            return relation.locationId;
+        });
+
+        const locations = await prisma.locations.findMany({
+            where: {
+                locationId: {in: locationIds}
+            }
+        })
+
+        res.json(locations)
+    } else {
+        res.json([])
+    }
 }
