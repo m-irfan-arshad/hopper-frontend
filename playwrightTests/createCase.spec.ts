6dcb660c015ef6dba46e259182c05d2c9cdc7d28
m_irfan_arshad <mirfanarshad17@gmail.com>
2022-12-01 14:51:40 -0500

    POM and login

--- playwrightTests/createCase.spec.ts
+++ playwrightTests/createCase.spec.ts
@@ -1,9 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { createPatientCase } from './dataGenerator';
+import { DashboardPage } from './dashboardPage';
 
 test.describe('Landing Page', () => {
     test.beforeEach(async ({ page, baseURL }) => {
+        const dashboard = new DashboardPage(page);
         await page.goto(baseURL!);
+        await dashboard.logIn();
     });
 
     test('Validating Create Case Modal=', async ({ page }) => {
@@ -15,9 +18,9 @@ test.describe('Landing Page', () => {
         const { birthDay } = createPatientCase();
         await createCaseButton.click();
         await expect(patientInformation).toBeVisible();
-        await page.locator('#firstName').type(firstName);
-        await page.locator('#lastName').type(lastName);
-        await page.locator('#dateOfBirth').type(birthDay);
+        await page.locator('//input[@id="patient.firstName"]').type(firstName);
+        await page.locator('//input[@id="patient.lastName"]').type(lastName);
+        await page.locator('//input[@id="patient.dateOfBirth"]').type(birthDay);
         await cancelButton.click();
         await expect(patientInformation).toBeHidden();
     });
