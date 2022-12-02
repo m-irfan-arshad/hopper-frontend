cf2bb8fa7120e6eefc10fc71424026f972721dd3
m_irfan_arshad <mirfanarshad17@gmail.com>
2022-12-02 13:00:17 -0500

    working alerts

---
+++ components/shared/alertComponent.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Alert, AlertColor, Fade } from "@mui/material";
+import {AlertContext} from "../../pages/_app"
+
+export default function AlertComponent() {
+  const [alertState, setAlertState] = useContext(AlertContext);
+
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      setAlertState({open: false, status: alertState.status, title: alertState.title})
+    }, 5000)
+
+    return () => {
+      clearTimeout(timeId)
+    }
+  }, [alertState.open]);
+
+  return (
+    <Fade in={alertState.open}>
+      <Alert sx={{position: 'fixed', bottom: 50, right: 50, zIndex: 10000, boxShadow: 1}}severity={alertState.status} onClose={() => setAlertState({open: false, status: alertState.status, title: alertState.title})}>{alertState.title}</Alert>
+    </Fade>
+  );
+}
