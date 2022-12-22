0c50ee59afaded3f22dbfa22ec3f348b9c2f4752
m_irfan_arshad <mirfanarshad17@gmail.com>
2022-12-22 16:53:58 -0500

    bugfix

--- pages/api/caseToPlatform.ts
+++ pages/api/caseToPlatform.ts
@@ -3,17 +3,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../prisma/clientInstantiation';
 import { validateParameters } from '../../utils/helpers';
 import {GoogleAuth} from 'google-auth-library';
-import * as dotenv from 'dotenv'
-dotenv.config()
 
 export default async function caseToPlatform(req: NextApiRequest, res: NextApiResponse) {
     
     console.log("GOOGLE_APPLICATION_CREDENTIALS: ", process.env.GOOGLE_APPLICATION_CREDENTIALS)
       const auth = new GoogleAuth();
-      console.log(await auth._getApplicationCredentialsFromFilePath(process.env.GOOGLE_APPLICATION_CREDENTIALS as string))
       const url = 'https://us-central1-mt-hp-s-dev-b7e1.cloudfunctions.net/hopper-case-to-platform'
       const creds = await auth.getIdTokenClient(url)
       console.log("creds: ", creds)
 
-    res.json({})
+    res.json({creds: creds})
 }
