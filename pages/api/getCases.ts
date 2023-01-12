0e9a086ed588b4083ccdb2a2c9967efb8dea53da
m_irfan_arshad <mirfanarshad17@gmail.com>
2023-01-12 12:31:43 -0500

    HPR-297: Add back try catch block to get cases

--- pages/api/getCases.ts
+++ pages/api/getCases.ts
@@ -8,40 +8,44 @@ import { withApiAuthRequired } from '@auth0/nextjs-auth0';
 const requiredParams = ['dateRangeStart', 'dateRangeEnd', 'page', 'orderBy'];
 
 export default withApiAuthRequired( withValidation(requiredParams, async function getCasesHandler(req: NextApiRequest, res: NextApiResponse) {
-  const dashboardParams = {
-    searchValue: <string>req.query["searchValue"],
-    dateRangeStart: <string>req.query["dateRangeStart"],
-    dateRangeEnd: <string>req.query["dateRangeEnd"],
-    vendorConfirmation: <string>req.query["vendorConfirmation"],
-    priorAuthorization: <string>req.query["priorAuthorization"],
-  };
+  try {
+    const dashboardParams = {
+      searchValue: <string>req.query["searchValue"],
+      dateRangeStart: <string>req.query["dateRangeStart"],
+      dateRangeEnd: <string>req.query["dateRangeEnd"],
+      vendorConfirmation: <string>req.query["vendorConfirmation"],
+      priorAuthorization: <string>req.query["priorAuthorization"],
+    };
 
-  const paginationSkipAmount = (parseInt(<string>req.query["page"]) - 1) * 50;
+    const paginationSkipAmount = (parseInt(<string>req.query["page"]) - 1) * 50;
 
-  const count =  await prisma.cases.count({
-    where: formatDashboardQueryParams(dashboardParams)
-  });
+    const count =  await prisma.cases.count({
+      where: formatDashboardQueryParams(dashboardParams)
+    });
 
-  const resultPosts = await prisma.cases.findMany({
-      where: formatDashboardQueryParams(dashboardParams),
-      take: paginationCount,
-      skip: paginationSkipAmount,
-      orderBy: [
-        {
-          procedureDate: <Prisma.SortOrder>req.query["orderBy"]
+    const resultPosts = await prisma.cases.findMany({
+        where: formatDashboardQueryParams(dashboardParams),
+        take: paginationCount,
+        skip: paginationSkipAmount,
+        orderBy: [
+          {
+            procedureDate: <Prisma.SortOrder>req.query["orderBy"]
+          }
+        ],
+        include: {
+            patients: true,
+            locations: true,
+            providers: true
         }
-      ],
-      include: {
-          patients: true,
-          locations: true,
-          providers: true
-      }
-  })
+    })
 
-  res.json({
-    cases: resultPosts.map(function(oldCase) {
-      return casesFormatter({cases: oldCase})
-    }), 
-    count: count
-  })
+    res.json({
+      cases: resultPosts.map(function(oldCase) {
+        return casesFormatter({cases: oldCase})
+      }), 
+      count: count
+    })
+  } catch(err) {
+    res.status(500).json({ message: err });
+}
 }))
